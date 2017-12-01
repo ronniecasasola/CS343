58089c5379a1550cc609622ba9010dd94273ecc0
xinyichen8 <xinyichen8@gmail.com>
2017-12-01 10:17:29 -0800

    create employee class

---
+++ src/cafe343/Employee.java
@@ -0,0 +1,19 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package cafe343;
+import java.util.*;
+/**
+ *
+ * @author xinyi
+ */
+public class Employee 
+{
+    private int employeeID;
+    private ArrayList<Integer> tableHelp;
+    private ArrayList<Customer> customers;
+    
+    
+}
