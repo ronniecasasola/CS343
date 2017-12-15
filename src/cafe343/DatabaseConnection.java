9be28c0b6faf1161042047f75c1a5b907876cab6
Ronnie Casasola <ronniecasasola@gmail.com>
2017-12-15 12:41:13 -0800

    Ronnie Database
    It's me

---
+++ src/cafe343/DatabaseConnection.java
@@ -0,0 +1,18 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package cafe343;
+
+/**
+ *
+ * @author Ronnie
+ */
+public class DatabaseConnection {
+
+    public static final String DB_URL = "jdbc:derby://localhost:1527/RestaurantSystem; UID=ronnie;PWD=casasola";
+    public static final String DB_USER = "ronnie";
+    public static final String DB_PASS = "casasola";
+
+}
